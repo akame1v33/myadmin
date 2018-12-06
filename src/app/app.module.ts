672f4d2dfbeb0963778da48310910de2432a8842
Kevin L. Fabian <kevin.fabian@pmti.local>
2018-12-06 16:18:57 +0800

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import { SidebarModule } from './sidebar/sidebar.module';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import  { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    NavbarModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
