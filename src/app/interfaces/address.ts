bfcd22cd815c7c7141fc43da9102b164b95eecef
Kevin L. Fabian <kevin.fabian@pmti.local>
2018-12-07 15:34:26 +0800

    updating

---
+++ src/app/interfaces/address.ts
@@ -0,0 +1,6 @@
+export interface Address {
+    address?:string;
+    city?:string;
+    province?:string;
+    barangay?:string;
+}
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,9 @@
+import { Name } from "./name";
+
+export interface User {
+    emailAddress?:string;
+    password?:string;
+    name?:Name;
+    mobileNo?:string;
+}
+
